724372991c5cb564b5537f21582f608c9ebe71b2
Ivan Kastle <bluehorizon186@gmail.com>
2024-12-01 13:28:11 -0800

    Login form submission works! It still doesn't do anything though :)

---
+++ app.js
@@ -0,0 +1,33 @@
+// File: app.js
+
+// Requires of modules!
+const express = require('express')
+const bodyParser = require('body-parser')
+
+// Constants!
+const app = express()
+const port = 3000
+
+// Use resources!
+app.use(express.static('views'))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Set Embedded JS for rendering the html pages!
+app.set('view engine', 'html')
+app.engine('html', require('ejs').renderFile)
+
+app.get('/login', function(request, response) {
+    response.render('login.html')
+})
+
+app.post('/validatelogin', function(request, response) {
+    console.log(request.body)
+})
+
+app.get('/', function(request, response) {
+    response.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log("First app running on port ${port}!")
+})
